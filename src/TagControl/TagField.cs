6b8e2d061e2740a56febfd08c1d5a74ab0cfb528
Ayman Barhoum <ayman@indivirtualdubai.com>
2017-06-26 02:00:46 +0400

    Add Try Catch

--- src/TagControl/TagField.cs
+++ src/TagControl/TagField.cs
@@ -26,7 +26,7 @@ namespace TagControl
 {
     public class TagField : Sitecore.Web.UI.HtmlControls.Control
     {
-       
+
         #region Private Properties
 
         private string HTMLTemplatePath
@@ -156,94 +156,125 @@ namespace TagControl
 
         void BuildTagsControl(System.Web.UI.WebControls.Literal literalTags)
         {
-            string tagEntitiesJson = string.Empty;
-            var tagEntities = new List<TagEntity>();
-            StringBuilder list = new StringBuilder();
-            var appDomain = AppDomain.CurrentDomain;
-            var basePath = appDomain.BaseDirectory;
-            var tags = TagsSearch();
-            var jsonSerialiser = new JavaScriptSerializer();
-            var tagsJson = jsonSerialiser.Serialize(tags);
-            var path = Path.Combine(basePath, HTMLTemplatePath);
-            var html = System.IO.File.ReadAllText(path);
-
-            html = html.Replace("($tags$)", tagsJson);
-
-            if (!string.IsNullOrEmpty(Value))
+            try
             {
+                string tagEntitiesJson = string.Empty;
+                var tagEntities = new List<TagEntity>();
+                StringBuilder list = new StringBuilder();
+                var appDomain = AppDomain.CurrentDomain;
+                var basePath = appDomain.BaseDirectory;
+                var tags = TagsSearch();
+                var jsonSerialiser = new JavaScriptSerializer();
+                var tagsJson = jsonSerialiser.Serialize(tags);
+                var path = Path.Combine(basePath, HTMLTemplatePath);
+                var html = System.IO.File.ReadAllText(path);
 
-                var tagList = jsonSerialiser.Deserialize<List<TagEntity>>(Value);
-                foreach (var tag in tagList)
+                html = html.Replace("($tags$)", tagsJson);
+
+                if (!string.IsNullOrEmpty(Value))
                 {
-                    var id = tag.id;
-                    if (id != "0")
+
+                    var tagList = jsonSerialiser.Deserialize<List<TagEntity>>(Value);
+                    foreach (var tag in tagList)
                     {
-                        ID parsedId;
-                        Sitecore.Data.ID.TryParse(id, out parsedId);
-                        if (!Sitecore.Data.ID.IsNullOrEmpty(parsedId))
+                        var id = tag.id;
+                        if (id != "0")
                         {
-                            var item = Client.ContentDatabase.GetItem(parsedId);
-                            if (item != null)
+                            ID parsedId;
+                            Sitecore.Data.ID.TryParse(id, out parsedId);
+                            if (!Sitecore.Data.ID.IsNullOrEmpty(parsedId))
                             {
-                                list.Append(string.Format("<li data-id='{0}'>{1}</li>", item.ID.ToString(), item[TitleField]));
-                                tagEntities.Add(new TagEntity { id = item.ID.ToString(), label = item[TitleField] });
+                                var item = Client.ContentDatabase.GetItem(parsedId);
+                                if (item != null)
+                                {
+                                    list.Append(string.Format("<li data-id='{0}'>{1}</li>", item.ID.ToString(), item[TitleField]));
+                                    tagEntities.Add(new TagEntity { id = item.ID.ToString(), label = item[TitleField] });
+                                }
                             }
                         }
                     }
-                }
 
-                tagEntitiesJson = jsonSerialiser.Serialize(tagEntities);
+                    tagEntitiesJson = jsonSerialiser.Serialize(tagEntities);
+                }
+                html = html.Replace("($avalilableTags$)", list.ToString());
+                html = html.Replace("($jsonObject$)", string.IsNullOrEmpty(tagEntitiesJson) ? "[]" : HttpUtility.HtmlEncode(tagEntitiesJson));
+                literalTags.Text = html;
             }
-            html = html.Replace("($avalilableTags$)", list.ToString());
-            html = html.Replace("($jsonObject$)", string.IsNullOrEmpty(tagEntitiesJson) ? "[]" : HttpUtility.HtmlEncode(tagEntitiesJson));
-            literalTags.Text = html;
+            catch (Exception ex)
+            {
+                Log.Error("TagField - BuildTagsControl method caused an unhandled exception", ex, this);
+            }
         }
 
         private Item[] GetTags()
         {
-            Item[] tags = Client.ContentDatabase.SelectItems(Source);
+            Item[] tags=null;
+            try
+            {
+                tags = Client.ContentDatabase.SelectItems(Source);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("TagField - GetTags method caused an unhandled exception", ex, this);
+            }
+
             return tags;
         }
 
         private List<TagEntity> TagsSearch()
         {
             var items = new List<TagEntity>();
-            if (!(Sitecore.Data.ID.IsNullOrEmpty(TagFolderId) || Sitecore.Data.ID.IsNullOrEmpty(TagTemplateId)))
+            try
             {
-                using (var context = ContentSearchManager.GetIndex("sitecore_master_index").CreateSearchContext())
+                if (!(Sitecore.Data.ID.IsNullOrEmpty(TagFolderId) || Sitecore.Data.ID.IsNullOrEmpty(TagTemplateId)))
                 {
-                    var predicate = PredicateBuilder.True<SearchResultItem>();
-                    predicate = predicate.And(p => p.Paths.Contains(TagFolderId));
-                    predicate = predicate.And(p => p.TemplateId == TagTemplateId);
-                    predicate = predicate.And(p => p.Language == Sitecore.Context.Language.Name);
-                    var result = context.GetQueryable<SearchResultItem>().Where(predicate);
-                    if (result != null && result.Count() > 0)
+                    using (var context = ContentSearchManager.GetIndex("sitecore_master_index").CreateSearchContext())
                     {
-                        var searchresultItems = result.ToList();
-                        foreach (var searchResultItem in searchresultItems)
+                        var predicate = PredicateBuilder.True<SearchResultItem>();
+                        predicate = predicate.And(p => p.Paths.Contains(TagFolderId));
+                        predicate = predicate.And(p => p.TemplateId == TagTemplateId);
+                        predicate = predicate.And(p => p.Language == Sitecore.Context.Language.Name);
+                        var result = context.GetQueryable<SearchResultItem>().Where(predicate);
+                        if (result != null && result.Count() > 0)
                         {
-                            var currentItem = searchResultItem.GetItem();
-                            items.Add(new TagEntity
+                            var searchresultItems = result.ToList();
+                            foreach (var searchResultItem in searchresultItems)
                             {
-                                id = currentItem.ID.ToString(),
-                                label = currentItem[TitleField]
-                            });
-                        }
+                                var currentItem = searchResultItem.GetItem();
+                                items.Add(new TagEntity
+                                {
+                                    id = currentItem.ID.ToString(),
+                                    label = currentItem[TitleField]
+                                });
+                            }
 
+                        }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("TagField - TagsSearch method caused an unhandled exception", ex, this);
             }
             return items;
         }
 
         public Item CreateItem(string itemName, Database contentDatabase)
         {
-          
-            Item parentItem = contentDatabase.GetItem(Source);
-            //Now we need to get the template from which the item is created
-            TemplateItem template = contentDatabase.GetTemplate(TagTemplateId);
-            //Now we can add the new item as a child to the parent
-            return parentItem.Add(itemName, template);
+            Item item = null;
+            try
+            {
+                Item parentItem = contentDatabase.GetItem(Source);
+                //Now we need to get the template from which the item is created
+                TemplateItem template = contentDatabase.GetTemplate(TagTemplateId);
+                //Now we can add the new item as a child to the parent
+                item = parentItem.Add(itemName, template);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("TagField - CreateItem method caused an unhandled exception", ex, this);
+            }
+            return item;
         }
 
         #endregion
